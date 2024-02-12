55dbc329b4206a5171837e950920f874d2eb3536
ChemaDvp <chemamartineztkd@gmail.com>
2024-02-12 14:13:18 +0100

    cambio color + imagenes

--- app/src/main/java/com/vedruna/proyectofinalmultimedia/utils/Constants.java
+++ app/src/main/java/com/vedruna/proyectofinalmultimedia/utils/Constants.java
@@ -1,6 +1,10 @@
 package com.vedruna.proyectofinalmultimedia.utils;
 
 public class Constants {
-    public static final String BASE_URL = "http://192.168.66.79:8080/";
-    //public static final String BASE_URL = "http://192.168.1.100:8080/";
+
+    // IP SHOGI
+    //public static final String BASE_URL = "http://192.168.66.79:8080/";
+
+    // IP PISO
+    public static final String BASE_URL = "http://192.168.1.100:8080/";
 }
