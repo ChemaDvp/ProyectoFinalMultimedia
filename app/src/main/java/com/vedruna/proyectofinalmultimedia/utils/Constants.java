3091f5c577f1b8668f89842372458d4918b087aa
ChemaDvp <chemamartineztkd@gmail.com>
2024-02-14 17:36:28 +0100

    cambio color y botones

--- app/src/main/java/com/vedruna/proyectofinalmultimedia/utils/Constants.java
+++ app/src/main/java/com/vedruna/proyectofinalmultimedia/utils/Constants.java
@@ -13,5 +13,6 @@ public class Constants {
     /**
      * La URL base para la conexión con el servidor en el PISO.
      */
-    public static final String BASE_URL = "http://192.168.1.100:8080/";
+    //public static final String BASE_URL = "http://192.168.1.100:8080/";
+    public static final String BASE_URL = "http://192.168.66.79:8080/";
 }
